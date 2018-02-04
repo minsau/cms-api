12d9df3c8acac0317e93776eefed0bf8de4154c9
Saúl Gómez <minsau2@gmail.com>
2018-02-04 12:42:54 -0600

    JWT Security added

--- app/User.php
+++ app/User.php
@@ -5,9 +5,19 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject as AuthenticatableUserContract;
+
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Authenticatable implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract,
+    AuthenticatableUserContract
 {
-    use Notifiable;
+    
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'is_verified'
     ];
 
     /**
@@ -26,4 +36,25 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();  // Eloquent model method
+    }
+
+    /**
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+             'user' => [ 
+                'id' => $this->id,
+                'name' => $this->name
+             ]
+        ];
+    }
 }
